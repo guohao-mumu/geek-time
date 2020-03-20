5c6660d28bd9f630fd8e3d162a808ee39dd2dabb
18810707531@163.com <18810707531@163.com>
2020-03-20 14:46:31 +0800

    '数组'

---
+++ src/main/java/com/gh/algorithm/Test.java
@@ -0,0 +1,17 @@
+package com.gh.algorithm;
+
+/**
+ * @Auther: guohao
+ * @Date: 2020/3/20 11:25
+ * @Description:
+ */
+public class Test {
+    public static void main(String[] args) {
+        int i = 0;
+        int[] arr = {0,0,0};
+        for (; i <= 3; i++) {
+            arr[i] = 0;
+            System.out.println("hello world\n");
+        }
+    }
+}
